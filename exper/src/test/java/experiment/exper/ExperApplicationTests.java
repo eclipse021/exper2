272048a7f7b320edfd8b6e67f01a652449b68feb
eclipse021 <junhyeon@naver.com>
2024-07-10 12:30:17 +0900

    SpringTest 어노테이션 삭제

--- exper/src/test/java/experiment/exper/ExperApplicationTests.java
+++ exper/src/test/java/experiment/exper/ExperApplicationTests.java
@@ -3,7 +3,6 @@ package experiment.exper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ExperApplicationTests {
 
 	@Test
